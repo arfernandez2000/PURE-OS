abd8a703fbaa42d5235052be3ca0d80bb4c5933c
mdedeu <mdedeu@itba.edu.ar>
2021-10-28 16:52:44 -0300

    Loop Process

--- Userland/SampleCodeModule/shell/commands/loop.c
+++ Userland/SampleCodeModule/shell/commands/loop.c
@@ -3,6 +3,8 @@
 #include "shell.h"
 #include "libc.h"
 
+#define NULL (void*) 0 
+
 
 int ticksElapsed()
 {
@@ -14,17 +16,26 @@ void waitCycles(int cycles)
       int goal = ticksElapsed() + cycles;
       while (ticksElapsed() < goal);
 }
-void loop(char* window, int * offset){
+
+void loopProc(int argc, char** argv){
 
     uint64_t pid = syscall(GET_PID, 0, 0, 0, 0, 0, 0);
-
     while (1)
     {
         waitCycles(10);
         char buff[20];
-        addText("Hello Nigerian Prince :)", window, offset);
+        char* window = argv[0];
+        int* offset = (int*) argv[1];
+        addText("Hello Nigerian Prince :) ", window, offset);
         addText(itoa(pid, buff, 10), window, offset);
         printWindow(window);
         substractLine(window, offset);
     }
+
 }
+
+void loop(char* window, int * offset){
+    char* argv[2] = {window, (char*) offset};
+    sys_loadProcess(&loopProc, 3, argv, 0, NULL, "loop");
+}
+
