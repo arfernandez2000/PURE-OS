dbf062e187b8c64973e45d153d088cb5d01357ee
Hernan Finucci <hfinucci@itba.edu.ar>
2021-10-30 02:04:51 -0300

    "funciona" ps (falta name y rbp)

--- Kernel/pcb.c
+++ Kernel/pcb.c
@@ -5,7 +5,6 @@
 #include "video.h"
 #include "naiveConsole.h"
 
-
 int activeProcesses = 0;
 int currentProcess = 0;
 
@@ -74,7 +73,7 @@ PCB* createPCB(void (*entryPoint)(int, char **), int argc, char **argv, int fg, 
     newProcess->state = READY;
     newProcess->priority = 1;
 
-    strcpy(newProcess->name, name);
+    strcpy(name, newProcess->name);
     //arrastre error 
     newProcessStack(entryPoint, argc, argv);
     return newProcess;
@@ -129,9 +128,27 @@ void printProcess(PCB *process)
               
 }
 
-PCB** psDisplay()
-{
-   return processQueue;
+//TODO Imprimir name y rbp
+char** psDisplay() {
+    char *processString[100];
+    processString[0][0] = 0;
+    strcpy(processQueue[0]->name ,processString[0]);
+    char buff[10];
+    for (int i = 0; i < activeProcesses; i++) {
+        // strcat(processString[i],processQueue[i]->name);
+        // strcat(processString[i],"   ");
+        strcat(processString[i], itoa(processQueue[i]->pid, buff, 10,2));
+        strcat(processString[i],"       ");
+        strcat(processString[i], itoa(processQueue[i]->priority, buff, 10,3));
+        strcat(processString[i],"       ");
+        strcat(processString[i], itoa(processQueue[i]->foreground, buff, 10,3));
+        strcat(processString[i],"       ");
+        strcat(processString[i],"0x");
+        strcat(processString[i], itoa(processesStack[i], buff, 10,8));
+        strcat(processString[i],"       ");
+        strcat(processString[i], itoa(processQueue[i]->state, buff, 10,3));
+    };
+    return processString;
 }
 
 
--- Userland/SampleCodeModule/shell/commands/processCommands.c
+++ Userland/SampleCodeModule/shell/commands/processCommands.c
@@ -1,36 +1,49 @@
 #include "system.h"
-#include "pcb.h"
 #include "shell.h"
 
 void ps() {
-    int proccessCount =  syscall(PROCCESS_COUNT, 0, 0, 0, 0, 0, 0);
-    PCB ** array = syscall(PS, 0, 0, 0, 0, 0, 0);
-    addText("Name PID PPID priority foreground rbp rsp state");
+    addText("Name      PID     priority    foreground     rbp          rsp          state");
     substractLine();
-    for(int i =0 ; i< proccessCount; i++){ 
-        addText(array[i]->name);
-        addText(' ');
-        addText(array[i]->pid);   
-        addText(' ');
-        addText(array[i]->ppid); 
-        addText(' ');
-        addText(array[i]->priority); 
-        addText(' ');
-        addText(array[i]->foreground); 
-        addText(' ');
-        addText(array[i]->rbp);
-        addText(' ');
-        addText(array[i]->rsp); 
-        addText(' ');
-        char pid[20];
-        addText(itoa(array[i]->pid, pid, 10));
-        addText(' ');
-        char state[20];
-        addText(itoa(array[i]->state, state, 10));
+    printWindow();
+    char** result = (char**) syscall(PS,0,0,0,0,0,0);
+    int processCount =  syscall(PROCESS_COUNT, 0, 0, 0, 0, 0, 0);
+    for (int i = 0; i < processCount; i++) {
+        addText(result[i]);
         substractLine();
+        printWindow();
     }
     
 }
+
+// void ps() {
+//     int proccessCount =  syscall(PROCCESS_COUNT, 0, 0, 0, 0, 0, 0);
+//     PCB ** array = syscall(PS, 0, 0, 0, 0, 0, 0);
+//     addText("Name PID PPID priority foreground rbp rsp state");
+//     substractLine();
+//     for(int i =0 ; i< proccessCount; i++){ 
+//         addText(array[i]->name);
+//         addText(' ');
+//         addText(array[i]->pid);   
+//         addText(' ');
+//         addText(array[i]->ppid); 
+//         addText(' ');
+//         addText(array[i]->priority); 
+//         addText(' ');
+//         addText(array[i]->foreground); 
+//         addText(' ');
+//         addText(array[i]->rbp);
+//         addText(' ');
+//         addText(array[i]->rsp); 
+//         addText(' ');
+//         char pid[20];
+//         addText(itoa(array[i]->pid, pid, 10));
+//         addText(' ');
+//         char state[20];
+//         addText(itoa(array[i]->state, state, 10));
+//         substractLine();
+//     }
+// }
+
 void kill(uint64_t pid){
     syscall(KILL, pid, 0, 0, 0, 0, 0);
 
