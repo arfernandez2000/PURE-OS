534bd44075be4500f1834c9db7338450deaea67d
mdedeu <mdedeu@itba.edu.ar>
2021-11-01 22:48:39 -0300

    Start Phylos

--- Userland/SampleCodeModule/shell/commands/phylo.c
+++ Userland/SampleCodeModule/shell/commands/phylo.c
@@ -1,24 +1,59 @@
-#define N 5 /* number of philosophers */
-#define LEFT (i+N−1)%N /* number of i’s left neighbor */
-#define RIGHT (i+1)%N /* number of i’s right neighbor */
-#define THINKING 0 /* philosopher is thinking */
-#define HUNGRY 1  /* philosopher is trying to get for ks */
-#define EATING 2 /* philosopher is eating */
+#include "phylo.h"
 
-// typedef int semaphore; /* semaphores are a special kind of int */
-// int state[N]; /* array to keep track of everyone’s state */
-// semaphore mutex = 1; /* mutual exclusion for critical regions */
-// semaphore s[N]; /* one semaphore per philosopher */
+#define INITIAL_PHYLOS 5 /* number of philosophers */
+#define MAX_PHYLOS 62
+#define LEFT (i+N−1)%N /* number of i’s left neighbor */ //TODO hacer esto dinamicamente
+#define RIGHT (i+1)%N /* number of i’s right neighbor */ //TODO hacer esto dinamicamente
 
-// void philosopher(int i) /* i: philosopher number, from 0 to N−1 */
-// {
-//     while (TRUE) { /* repeat forever */
-//         think( ); /* philosopher is thinking */
-//         take_forks(i); /* acquire two for ks or block */
-//         eat( ); /* yum-yum, spaghetti */
-//         put_forks(i); /* put both for ks back on table */
-//     }
-// }
+#define THINKING 0 
+#define HUNGRY 1  
+#define EATING 2 
+
+
+typedef int semaphore; /* semaphores are a special kind of int */
+int state[MAX_PHYLOS]; /* array to keep track of everyone’s state */
+semaphore mutex = 1; /* mutual exclusion for critical regions */
+semaphore s[MAX_PHYLOS]; /* one semaphore per philosopher */
+
+int phylosCount = INITIAL_PHYLOS;
+
+void addInitialPhylos(int fg){
+    char buffer[10];
+    char *argv[] = { "phylosopher", itoa(fg,buffer,10)};
+    for(int i=0; i< N; i++){
+        int error = sys_loadProcess(&philosopher,2,argv,fg,0);
+        if(error == -1){
+        addText("Error Loading Phylosopher");
+        substractLine();
+        printWindow();
+        return;
+        }
+    }
+}
+
+void table(int argc, char** argv){
+    addInitialPhylos(atoi(argv[0],2));
+    while(1){
+        
+    }
+
+}
+static void think(){
+  return;
+}
+static void eat(){
+    return;
+}
+
+void philosopher(int i) /* i: philosopher number, from 0 to N−1 */
+{
+    while (1) { /* repeat forever */
+        think(); /* philosopher is thinking */
+        //take_forks(i); /* acquire two for ks or block */
+        eat(); /* yum-yum, spaghetti */
+        //put_forks(i); /* put both for ks back on table */
+    }
+}
 
 // void take_forks(int i) /* i: philosopher number, from 0 to N−1 */
 // {
@@ -51,5 +86,16 @@ void phylo(int fg){
     //proceso para cada uno de los filosofos iniciales
     //que cada filosofo imprima su estado?
     //ver en fg y en bg
-    return;
+    char buffer[10];
+    char * argv[] = {"Phylosophers table", itoa(fg, buffer,10)};
+    int error = sys_loadProcess(&table,2,argv,0,0);
+    if(error == -1){
+        addText("Error Loading Table");
+        substractLine();
+        printWindow();
+        return;
+    }
+
+
+
 }
--- Userland/SampleCodeModule/shell/include/phylo.h
+++ Userland/SampleCodeModule/shell/include/phylo.h
@@ -1,6 +1,9 @@
 #ifndef phylo_h
 #define phylo_h
-    
+
+#include "system.h"
+#include "libc.h"
+#include "shell.h"
 void phylo( int fg);
 
 #endif
