9f2c9121c6d6fe4fde154f9ec64f2eaa5ba7d85a
Hernan Finucci <hfinucci@itba.edu.ar>
2021-11-03 11:32:47 -0300

    phylo fg finished

--- Userland/SampleCodeModule/shell/commands/phylo.c
+++ Userland/SampleCodeModule/shell/commands/phylo.c
@@ -3,130 +3,137 @@
 #define INITIAL_PHYLOS 4 /* number of philosophers */
 #define MAX_PHYLOS 9
 
-
-#define THINKING 0 
-#define HUNGRY 1  
-#define EATING 2 
-
+#define THINKING 0
+#define HUNGRY 1
+#define EATING 2
 
 typedef int semaphore; /* semaphores are a special kind of int */
 int state[MAX_PHYLOS]; /* array to keep track of everyone’s state */
-semaphore mutex = 1; /* mutual exclusion for critical regions */
+semaphore mutex = 1;   /* mutual exclusion for critical regions */
 semaphore mutexTable = 1;
 semaphore s[MAX_PHYLOS]; /* one semaphore per philosopher */
 int phylosPid[MAX_PHYLOS];
 
 int phylosCount = 0;
 
-#define RIGHT(i) ((i) + 1) % (phylosCount)                         /* number of i’s right neighbor */
+#define RIGHT(i) ((i) + 1) % (phylosCount)              /* number of i’s right neighbor */
 #define LEFT(i) ((i) + phylosCount - 1) % (phylosCount) /* number of i’s left neighbor */
+
+int tablePrintID = -1;
 
 void sleep(unsigned int seconds)
 {
-      unsigned int limitTime = seconds + ticksElapsed();
-      while (ticksElapsed() < limitTime)
-            ;
+    unsigned int limitTime = seconds + ticksElapsed();
+    while (ticksElapsed() < limitTime)
+        ;
 }
-void think(){
+void think()
+{
     sleep(10);
     return;
 }
 
-void eat(){
+void eat()
+{
     sleep(10);
     return;
 }
-static void up(int * sem){
+static void up(int *sem)
+{
     (*sem)++;
 }
-static void down(int * sem){
+static void down(int *sem)
+{
     (*sem)--;
 }
-void test(int i){
-    if (state[i] == HUNGRY && state[LEFT(i)] != EATING && state[RIGHT(i)] != EATING) {
+void test(int i)
+{
+    if (state[i] == HUNGRY && state[LEFT(i)] != EATING && state[RIGHT(i)] != EATING)
+    {
         state[i] = EATING;
         up(&s[i]);
     }
 }
 
-void take_forks(int i) 
+void take_forks(int i)
 {
 
-    down(&mutex); /* enter critical region */
+    down(&mutex);      /* enter critical region */
     state[i] = HUNGRY; /* record fact that philosopher i is hungry */
-    test(i); /* try to acquire 2 for ks */
-    up(&mutex); /* exit critical region */
-    down(&s[i]); /* block if for ks were not acquired */
+    test(i);           /* try to acquire 2 for ks */
+    up(&mutex);        /* exit critical region */
+    down(&s[i]);       /* block if for ks were not acquired */
 }
 
-void put_forks(int i) 
+void put_forks(int i)
 {
-    down(&mutex); /* enter critical region */
+    down(&mutex);        /* enter critical region */
     state[i] = THINKING; /* philosopher has finished eating */
-    test(LEFT(i)); /* see if left neighbor can now eat */
-    test(RIGHT(i)); /* see if right neighbor can now eat */
-    up(&mutex); /* exit critical region */
+    test(LEFT(i));       /* see if left neighbor can now eat */
+    test(RIGHT(i));      /* see if right neighbor can now eat */
+    up(&mutex);          /* exit critical region */
 }
 
-void philosopher(int argc, char ** argv)
+void philosopher(int argc, char **argv)
 {
-    int i = atoi(argv[2],1); // esto cambiarlo. HAY QUE RECIBIR POR PARAMETRO EL IDX DEL phylosopher
-    while (1) { /* repeat forever */
+    int i = atoi(argv[2], 1); // esto cambiarlo. HAY QUE RECIBIR POR PARAMETRO EL IDX DEL phylosopher
+    while (1)
+    { /* repeat forever */
         //think(); /* philosopher is thinking */
         take_forks(i); /* acquire two for ks or block */
         //eat(); /* yum-yum, spaghetti */
         put_forks(i); /* put both for ks back on table */
     }
 }
-int removePhylo(){
+int removePhylo()
+{
     down(&mutex);
-    if(phylosCount == INITIAL_PHYLOS){
+    if (phylosCount == INITIAL_PHYLOS)
+    {
         return -1;
     }
     phylosCount--;
     kill(phylosPid[phylosCount]);
     up(&mutex);
     return 1;
-
 }
-int addPhylo(int fg, int table){
-    if(phylosCount == MAX_PHYLOS)
+int addPhylo(int fg, int table)
+{
+    if (phylosCount == MAX_PHYLOS)
         return -1;
     down(&mutex);
     char buffer[10];
     char buffer2[10];
     char buffer3[10];
-    char *argv[] = { "phylo", itoa(fg, buffer,10), itoa(table,buffer2,10), itoa(phylosCount,buffer3,10)};
-    int pid = sys_loadProcess(&philosopher,4, argv, fg, 0); 
-    
-    if(pid == -1){
+    char *argv[] = {"phylo", itoa(fg, buffer, 10), itoa(table, buffer2, 10), itoa(phylosCount, buffer3, 10)};
+    int pid = sys_loadProcess(&philosopher, 4, argv, fg, 0);
+
+    if (pid == -1)
+    {
         addText("Error Loading Phylosopher");
         substractLine();
         printWindow();
         return -1;
     }
     state[phylosCount] = THINKING;
-    s[phylosCount] = 1; 
+    s[phylosCount] = 1;
     phylosPid[phylosCount++] = pid;
     up(&mutex);
     return 1;
 }
 
-
-
-void table(int argc, char** argv){
-    if(atoi(argv[0],10) == 1){
-        block(0);
-    }
-    int run = 1;
-    while(run){
+void tablePrint(int argc, char** argv) {
+    while (1) {
         sleep(10);
         down(&mutexTable);
-        for(int i=0; i< phylosCount; i++){
-            if(state[i] == EATING){
+        for (int i = 0; i < phylosCount; i++)
+        {
+            if (state[i] == EATING)
+            {
                 addText(" E ");
             }
-            else{
+            else
+            {
                 addText(" . ");
             }
         }
@@ -134,55 +141,43 @@ void table(int argc, char** argv){
         printWindow();
         up(&mutexTable);
     }
-
-    exit();
-}
-void killAllPhylos(){
-    for(int i=0; i< phylosCount; i++)
-        kill(phylosPid[i]);
 }
 
-//---------------------------------------------------
-void phylo(int fg){
-    int run = 1;
-    char buffer[10];
-    char * argv[] = {"table", itoa(fg, buffer,10)};
-    int tableID = sys_loadProcess(&table,2,argv,fg,0);
-    if(tableID == -1){
-        addText("Error Loading Table");
-        substractLine();
-        printWindow();
-        return;
+void table(int argc, char **argv)
+{
+    if (atoi(argv[0], 1) == 1)
+    {
+        block(0);
     }
-   block(tableID);
-   for(int i=0; i< INITIAL_PHYLOS; i++){
-       addPhylo(fg,tableID);
-   }
-   sleep(10);
-   unblock(tableID);
-
-   while(run){
-       char key = getChar();
+    int run = 1;
+    while (run)
+    {
+        char key = getChar();
         switch (key)
         {
         case 'a':
-            if (addPhylo(fg,tableID) == -1){
+            if (addPhylo(atoi(argv[0], 1), syscall(GET_PID,0,0,0,0,0,0)) == -1)
+            {
                 addText("Can\'t add another philosopher. Maximum 9 philosophers.");
                 substractLine();
                 printWindow();
-            }else{
+            }
+            else
+            {
                 addText("A new philosopher joined!");
                 substractLine();
                 printWindow();
             }
-            break;  
+            break;
         case 'd':
-            if (removePhylo() == -1){
+            if (removePhylo() == -1)
+            {
                 addText("Can\'t remove another philosopher. Minimum 4 philosophers.");
                 substractLine();
                 printWindow();
             }
-            else{
+            else
+            {
                 addText("One philosopher has left!");
                 substractLine();
                 printWindow();
@@ -194,12 +189,44 @@ void phylo(int fg){
         default:
             break;
         }
-   }
-   addText("Finished.");
-   substractLine();
-   printWindow();
-   killAllPhylos();
-   kill(tableID);
+    }
+    addText("Finished");
+    substractLine();
+    printWindow();
+    unblock(0);
+    kill(tablePrintID);
+    killAllPhylos();
+    addText("$> ");
+    printWindow();
+    exit();
+}
+void killAllPhylos()
+{
+    for (int i = 0; i < phylosCount; i++)
+        kill(phylosPid[i]);
+}
 
-
+//---------------------------------------------------
+void phylo(int fg)
+{
+    int run = 1;
+    char buffer[10];
+    char *argv[] = {"table", itoa(fg, buffer, 10)};
+    int tableID = sys_loadProcess(&table, 2, argv, fg, 0);
+    char *argv2[] = {"tableP", itoa(fg, buffer,10)};
+    tablePrintID = sys_loadProcess(&tablePrint, 2, argv2, fg, 0);
+    if (tableID == -1)
+    {
+        addText("Error Loading Table");
+        substractLine();
+        printWindow();
+        return;
+    }
+    block(tableID);
+    for (int i = 0; i < INITIAL_PHYLOS; i++)
+    {
+        addPhylo(fg, tableID);
+    }
+    sleep(10);
+    unblock(tableID);
 }
