5597917252979a626a24b253b7e347eb754158a0
mdedeu <mdedeu@itba.edu.ar>
2021-11-04 20:08:57 -0300

    CASI LISTO Co-authored-by: arfernandez2000 <arfernandez2000@users.noreply.github.com>

--- Userland/SampleCodeModule/shell/commands/files.c
+++ Userland/SampleCodeModule/shell/commands/files.c
@@ -2,14 +2,14 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "files.h"
 #include "libc.h"
-#include "pipesLib.h"
-#include "processCommands.h"
-#include "sem.h"
 #include "shell.h"
+#include "processCommands.h"
 #include "system.h"
-#include <fcntl.h>
-#include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "sem.h"
+#include "pipesLib.h"
 
 #define BUFF_SIZE 1024
 
@@ -21,8 +21,8 @@
 #define COLS 80
 #define ROWS 25
 
-// pipe 0 write
-// pipe 1 read
+//pipe 0 write
+//pipe 1 read
 
 int openFile(char *file);
 
@@ -34,11 +34,10 @@ int isVow(char c);
 
 void cat(int fg, int *pipes)
 {
-    
     char buffer[10];
     if(pipes[0] >= 0 || (fg && pipes[0] == -1 && pipes[1] == -1)){
-      substractLine();
-      printWindow();
+        substractLine();
+        printWindow();
         if(sOpen(SEM_SHELL, -1) == -1)
             return;
     }
@@ -90,7 +89,7 @@ void catProc(int argc, char **argv)
         exit();
     }
     else if (pipes[0] == -1 && pipes[1] >= 0) {
-        int i;
+        int i = 0;
         char* c;
         strcpy2(pRead(pipes[1]), c);
         while (*c != '\0')
@@ -107,90 +106,80 @@ void catProc(int argc, char **argv)
     addText(buffer);
     substractLine();
     printWindow();
-    pWrite(pipes[0], buffer);
-    exit();
-  } else if (pipes[0] == -1 && pipes[1] >= 0) {
-    int i;
-    char *c;
-    strcpy2(pRead(pipes[1]), c);
-    while (*c != '\0') {
-      buffer[i++] = c[0];
-      strcpy2(pRead(pipes[1]), c);
-    }
-    buffer[i] = '\0';
-  } else {
-    scanning(buffer, 0);
-  }
-  substractLine();
-  addText(buffer);
-  substractLine();
-  printWindow();
-  if (sPost(SEM_SHELL) == -1) {
-    return;
-  };
-  if (sClose(SEM_SHELL) == -1) {
-    return;
-  };
-  exit();
-}
-
-void scanning(char *buffer, int filterVow) {
-  char c;
-  int i = 0;
-  char *window = getWindow();
-  int offset = getOffset();
-  while ((c = getChar()) != '\t' && i < BUFF_SIZE - 1) {
-    if (c != -1) {
-      if (c == '\b' && i > 0) {
-        buffer[--i] = ' ';
-        window[--offset] = ' ';
-        setOffset(offset);
-        printWindow();
-      }
-      if (c == '\n') {
-        buffer[i++] = c;
-        substractLine();
-        offset = getOffset();
-        printWindow();
-      } else if (c != 0 && c != '\b') {
-        if (!filterVow || !isVow(c)) {
-          buffer[i++] = c;
-        }
-        if (offset == ROWS * COLS - 1) {
-          substractLine();
-          offset = getOffset();
-        }
-        window[offset++] = c;
-        setOffset(offset);
-        printWindow();
-      }
-    }
-  }
-  buffer[i] = '\0';
-  if (offset == ROWS * COLS - 1)
-    substractLine();
-  window[offset] = ' ';
-}
-
-void wc(int fg, int *pipes) {
-  substractLine();
-  printWindow();
-  char buffer[10];
-  if (fg && (pipes[0] >= 0 || (pipes[0] == -1 && pipes[1] == -1))) {
-    substractLine();
-    printWindow();
-    if (sOpen(SEM_SHELL, -1) == -1)
-      return;
-  }
-  char *argv[] = {"wc", itoa(fg, buffer, 10)};
-  int error = sys_loadProcess(&wcProc, 2, argv, fg, pipes);
-  if (error == -1) {
-    return;
-  }
-  if (fg && (pipes[0] >= 0 || (pipes[0] == -1 && pipes[1] == -1)))
-    if (sWait(SEM_SHELL) == -1) {
+    if(sPost(SEM_SHELL)==-1){
       return;
     };
+    if(sClose(SEM_SHELL) == -1){
+      return;
+    };
+    exit();
+}
+
+void scanning(char *buffer, int filterVow)
+{
+    char c;
+    int i = 0;
+    char *window = getWindow();
+    int offset = getOffset();
+    while ((c = getChar()) != '\t' && i < BUFF_SIZE - 1)
+    {
+        if (c != -1)
+        {
+            if (c == '\b' && i > 0)
+            {
+                buffer[--i] = ' ';
+                window[--offset] = ' ';
+                setOffset(offset);
+                printWindow();
+            }
+            if (c == '\n')
+            {
+                buffer[i++] = c;
+                substractLine();
+                offset = getOffset();
+                printWindow();
+            }
+            else if (c != 0 && c != '\b')
+            {
+                if (!filterVow || !isVow(c))
+                {
+                    buffer[i++] = c;
+                }
+                if (offset == ROWS * COLS - 1)
+                {
+                    substractLine();
+                    offset = getOffset();
+                }
+                window[offset++] = c;
+                setOffset(offset);
+                printWindow();
+            }
+        }
+    }
+    buffer[i] = '\0';
+    if (offset == ROWS * COLS - 1)
+        substractLine();
+    window[offset] = ' ';
+}
+
+void wc(int fg, int *pipes)
+{
+    char buffer[10];
+    if(fg && (pipes[0] >= 0 || (pipes[0] == -1 && pipes[1] == -1))){
+        substractLine();
+        printWindow();
+        if(sOpen(SEM_SHELL, -1) == -1)
+            return;
+    }
+    char* argv[] = {"wc", itoa(fg, buffer,10)};
+    int error = sys_loadProcess(&wcProc, 2, argv, fg, pipes);
+    if(error == -1){
+      return;
+    }
+    if(fg && (pipes[0] >= 0 || (pipes[0] == -1 && pipes[1] == -1)))
+        if(sWait(SEM_SHELL) == -1){
+          return;
+        };
 }
 
 void wcProc(int argc, char **argv)
@@ -224,61 +213,66 @@ void wcProc(int argc, char **argv)
 
     char buffer[BUFF_SIZE] = {0};
 
-  if (pipes[0] == -1 && pipes[1] >= 0) {
-    int i;
-    char *c;
-    strcpy2(pRead(pipes[1]), c);
-    while (*c != '\0') {
-      buffer[i++] = c[0];
-      strcpy2(pRead(pipes[1]), c);
+    if (pipes[0] == -1 && pipes[1] >= 0) {
+        int i = 0 ;
+        char* c;
+        strcpy2(pRead(pipes[1]), c);
+        while (*c != '\0')
+        {
+            buffer[i++] = c[0];
+            strcpy2(pRead(pipes[1]), c);
+        }
+        
+        buffer[i] = '\0';
     }
-
-    buffer[i] = '\0';
-  } else {
-    scanning(buffer, 0);
-  }
-  substractLine();
-  int lines = 1;
-  for (size_t i = 0; buffer[i] != '\0'; i++) {
-    if (buffer[i] == '\n') {
-      lines++;
+    else {
+        scanning(buffer, 0);
     }
-  }
-  char ret[BUFF_SIZE] = {0};
-  if (pipes[0] >= 0) {
-    pWrite(pipes[0], itoa(lines, ret, 10));
-    exit();
-  }
-  addText(itoa(lines, ret, 10));
-  substractLine();
-  printWindow();
-  if (sPost(SEM_SHELL) == -1) {
-    return;
-  }
-  if (sClose(SEM_SHELL) == -1) {
-    return;
-  }
-  exit();
-}
-
-void filter(int fg, int *pipes) {
-
-  char buffer[10];
-  if (fg && (pipes[0] >= 0 || (pipes[0] == -1 && pipes[1] == -1))) {
+    substractLine();
+    int lines = 1;
+    for (size_t i = 0; buffer[i] != '\0'; i++)
+    {
+        if (buffer[i] == '\n')
+        {
+            lines++;
+        }
+    }
+    char ret[BUFF_SIZE] = {0};
+    if(pipes[0] >= 0){
+       pWrite(pipes[0], itoa(lines, ret, 10));
+        exit(); 
+    }
+    addText(itoa(lines, ret, 10));
     substractLine();
     printWindow();
-    if (sOpen(SEM_SHELL, -1) == -1)
-      return;
-  }
-  char *argv[] = {"filter", itoa(fg, buffer, 10)};
-  int error = sys_loadProcess(&filterProc, 2, argv, fg, pipes);
-  if (error == -1) {
-    return;
-  }
-  if (fg && (pipes[0] >= 0 || (pipes[0] == -1 && pipes[1] == -1)))
-    if (sWait(SEM_SHELL) == -1) {
+    if(sPost(SEM_SHELL) == -1){
+        return;
+    }
+    if(sClose(SEM_SHELL) == -1){
+        return;
+    }
+    exit();
+}
+
+void filter(int fg, int *pipes)
+{
+    char buffer[10];
+    if(fg && (pipes[0] >= 0 || (pipes[0] == -1 && pipes[1] == -1))){
+        substractLine();
+        printWindow();
+        if(sOpen(SEM_SHELL, -1) == -1)
+            return;
+    }
+    char* argv[] = {"filter", itoa(fg, buffer,10)};
+    int error = sys_loadProcess(&filterProc, 2, argv, fg, pipes);
+    if(error == -1){
       return;
     }
+    if(fg && (pipes[0] >= 0 || (pipes[0] == -1 && pipes[1] == -1)))
+        if(sWait(SEM_SHELL) == -1){
+        return;
+    }
+
 }
 
 void filterProc(int argc, char **argv)
@@ -340,29 +334,11 @@ void filterProc(int argc, char **argv)
     if(sClose(SEM_SHELL) == -1){
         return;
     }
-
-    buffer[i] = '\0';
-  } else {
-    scanning(buffer, 1);
-  }
-  substractLine();
-  if (pipes[0] >= 0) {
-    pWrite(pipes[0], buffer);
     exit();
-  }
-  addText(buffer);
-  substractLine();
-  printWindow();
-  if (sPost(SEM_SHELL) == -1) {
-    return;
-  }
-  if (sClose(SEM_SHELL) == -1) {
-    return;
-  }
-  exit();
 }
 
-int isVow(char c) {
-  return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-          c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+int isVow(char c)
+{
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+            c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 }
